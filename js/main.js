609a1fec48eae4485adb7cd754772e30c92a49d4
Jasper Boyd <jasperboyd@lavabit.com>
2013-04-13 13:57:54 -0400

    worked on the lyrics script some more

--- js/main.js
+++ js/main.js
@@ -20,16 +20,33 @@ $(document).ready(function(){
 	$CB_LYR = $('#CB_LYR');
 	$NWIF_LYR = $('#NWIF_LYR');
 	
+	open = -1; 
+	
 	
 	$TOW_LK.click(function(){
-		$TOW_LYR.fadeToggle();
+		if (open === -1){ 
+			$TOW_LK.toggleClass("selected");
+			$TOW_LYR.fadeToggle();
+		} else if (open === 0) { 
+		
+		} else if (open === 1) { 
+		
+		} else if (open === 2) {
+		
+		} else if (open === 3) { 
+		
+		} else if (open === 4) { 
+		
+		} 
 	});
 	
 	$CB_LK.click(function(){
+		$CB_LK.toggleClass("selected");
 		$CB_LYR.fadeToggle();
 	});
 	
 	$NWIF_LK.click(function(){
+		$NWIF_LK.toggleClass("selected");
 		$NWIF_LYR.fadeToggle(); 
 	});
 	
